fd2f7e05e76b6860d72ce76c1957613feda69991
Eloise <eloisesharpe2708@gmail.com>
2026-08-22 12:03:24 +0100

    Method to get available mappool slots sorted

--- repository/utils.py
+++ repository/utils.py
@@ -17,3 +17,23 @@ def path_to_osz_template(slot: str) -> str:
         raise OSError(".osz template not found")
 
     return path
+
+def get_slots() -> list[str]:
+
+    osu_dir = Path() / "repository" / "osu"
+    files = osu_dir.iterdir()
+    
+    slots = [f.stem for f in files]
+    slots_sorted: list[str] = list()
+
+    groups = ["NM", "HD", "HR", "FM", "EX"]
+
+    for group in groups:
+        
+        slots_in_group = [s for s in slots if s.startswith(group)]
+        list.sort(slots_in_group)
+        
+        slots_sorted += slots_in_group
+
+    return slots_sorted
+
