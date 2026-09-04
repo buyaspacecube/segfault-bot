7529eea145a591a43b95a99be38f11a5cba74a54
Eloise <eloisesharpe2708@gmail.com>
2026-09-04 14:18:46 +0100

    Move commands out of main file

---
+++ interface/generate.py
@@ -0,0 +1,58 @@
+from discord import slash_command, SlashCommandGroup
+from discord.ext.commands import Cog
+
+from utils.discord_utils import get_slot_option, get_generated_seeds_message
+from utils.RNG_utils import generate_seeds
+from generator.generate import generate
+
+slot_option = get_slot_option()
+
+class Generate(Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @slash_command(name="generate", description="Generate a difficulty of the given slot")
+    async def command_generate(self, ctx,
+                               slot: slot_option):
+
+        seeds = generate_seeds(1)
+
+        message: str = get_generated_seeds_message(seeds)
+        osz: File = generate(slot, seeds)
+        
+        await ctx.respond(message, file=osz)
+
+    practice = SlashCommandGroup(name="practice", description="Generate difficulties without anyone else seeing")
+
+    @practice.command(name="diffs", description="Generate a number of difficulties to practice")
+    async def command_practice_diffs(self, ctx,
+                             slot: slot_option,
+                             diffs: int):
+
+        seeds = generate_seeds(diffs)
+
+        message: str = get_generated_seeds_message(seeds)
+        osz: File = generate(slot, seeds)
+        
+        await ctx.respond(message, file=osz, ephemeral=True)
+
+    @practice.command(name="seed", description="Generate a specific seed to practice")
+    async def command_practice_seed(self, ctx,
+                            slot: slot_option,
+                            seed: str):
+
+        seed_hex = int(seed, 16)
+        seeds = [seed_hex]
+
+        message: str = get_generated_seeds_message(seeds)
+        osz: File = generate(slot, seeds)
+        
+        await ctx.respond(message, file=osz, ephemeral=True)
+
+# add to bot
+def setup(bot):
+
+    generate = Generate(bot)
+    bot.add_cog(generate)
+
--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-from discord import Bot, ApplicationContext, File
+from discord import Bot
 
 from dotenv import load_dotenv
 from os import getenv
-
-from utils.discord_utils import get_slot_option, get_generated_seeds_message
-from utils.RNG_utils import generate_seeds
-from generator.generate import generate
 
 bot = Bot()
 
@@ -13,54 +9,8 @@ bot = Bot()
 async def on_ready():
     print(f"{bot.user} is alive")
 
-slot_option = get_slot_option()
+bot.load_extension('interface.generate')
 
-#
-# generate command (for referees)
-#
-@bot.command(name="generate", description="Generate a difficulty of the given slot")
-async def bot_generate(ctx: ApplicationContext,
-                       slot: slot_option):
-
-    seeds = generate_seeds(1)
-
-    message: str = get_generated_seeds_message(seeds)
-    osz: File = generate(slot, seeds)
-    
-    await ctx.respond(message, file=osz)
-
-#
-# practice commands (for players)
-#
-practice = bot.create_group(name="practice", description="Generate difficulties without anyone else seeing")
-
-@practice.command(name="diffs", description="Generate a number of difficulties to practice")
-async def bot_practice_diffs(ctx: ApplicationContext,
-                       slot: slot_option,
-                       diffs: int):
-
-    seeds = generate_seeds(diffs)
-
-    message: str = get_generated_seeds_message(seeds)
-    osz: File = generate(slot, seeds)
-    
-    await ctx.respond(message, file=osz, ephemeral=True)
-
-@practice.command(name="seed", description="Generate a specific seed to practice")
-async def bot_practice_seed(ctx: ApplicationContext,
-                       slot: slot_option,
-                       seed: str):
-
-    seeds = [int(seed, 16)]
-
-    message: str = get_generated_seeds_message(seeds)
-    osz: File = generate(slot, seeds)
-    
-    await ctx.respond(message, file=osz, ephemeral=True)
-
-#
-# run
-#
 load_dotenv()
 token = getenv('TOKEN')
 bot.run(token)
