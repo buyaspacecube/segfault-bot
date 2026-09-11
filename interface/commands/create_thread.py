2504d2a7f182df8079d3be476637bc99728e465a
Eloise <eloisesharpe2708@gmail.com>
2026-09-11 15:56:26 +0100

    DM match seeds to referee

--- interface/commands/create_thread.py
+++ interface/commands/create_thread.py
@@ -1,8 +1,10 @@
 from discord import slash_command, SlashCommandGroup
 from discord.ext.commands import Cog
 
+from generator.seeds import get_match_seeds
 from interface.options import get_player_option, get_streamer_option
 from interface.permissions import get_referee_permissions
+from hexadecimal.int_to_hex_string import int_to_hex_string
 
 player1_option, player2_option = get_player_option(1), get_player_option(2)
 streamer_option = get_streamer_option()
@@ -14,6 +16,15 @@ match_start_string = """
 
 {ref} will be your referee
 {stream} will be your streamer
+"""
+
+referee_seeds_string = """
+Seeds for **{title}**
+Copy these using the copy button at the top right, then paste onto the ref sheet!
+
+```
+{seeds}
+```
 """
 
 class CreateThread(Cog):
@@ -35,6 +46,11 @@ class CreateThread(Cog):
 
         referee = ctx.author
 
+        match_title = f"{player1.nick} vs. {player2.nick}"
+
+        #
+        # start match
+        #
         match_start_string_formatted = match_start_string.format(
             p1 = player1.mention,
             p2 = player2.mention,
@@ -43,7 +59,25 @@ class CreateThread(Cog):
         )
 
         message = await ctx.send(match_start_string_formatted)
-        await message.create_thread(name=f"{player1.nick} vs. {player2.nick}")
+        await message.create_thread(name=match_title)
+
+        #
+        # DM seeds to referee
+        #
+        match_ID = message.id
+        seeds: dict[str, int] = get_match_seeds(match_ID)
+
+        seeds_string = '\n'.join([
+            int_to_hex_string(seed)
+            for _, seed in seeds.items()
+        ])
+
+        referee_seeds_string_formatted = referee_seeds_string.format(
+            title = match_title,
+            seeds = seeds_string
+        )
+
+        await referee.send(referee_seeds_string_formatted)
 
 # add to bot
 def setup(bot):
