9bf51dfd243d09e8367ac4b68664a66c93ce883f
Eloise <eloisesharpe2708@gmail.com>
2026-09-18 21:47:34 +0100

    Fix blank permissions being weird

--- interface/permissions.py
+++ interface/permissions.py
@@ -7,16 +7,28 @@ def _get_permissions_if_playtesting():
 
     load_dotenv()
 
-    if getenv('PLAYTESTING') == 1:
+    if getenv('PLAYTESTING') == '1':
         return Permissions(create_private_threads = True)
 
     return Permissions()
 
 def get_base_permissions():
-    return Permissions() + _get_permissions_if_playtesting()
+    
+    perms = Permissions() + _get_permissions_if_playtesting()
+
+    if perms.value == 0:
+        return None
+    
+    return perms
 
 def get_referee_permissions():
-    return Permissions(create_public_threads = True) + _get_permissions_if_playtesting()
+    
+    perms = Permissions(create_public_threads = True) + _get_permissions_if_playtesting()
+    
+    if perms.value == 0:
+        return None
+    
+    return perms
 
 def get_admin_permissions():
     return Permissions(administrator = True)
